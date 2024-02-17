73e383782643bb5802aec47076842704895506a0
untic-dam <damien_modeling@outlook.fr>
2024-02-17 18:43:35 +0100

    clean pydantic schema of candidate

--- schema/candidate_schema.py
+++ schema/candidate_schema.py
@@ -2,32 +2,37 @@ from typing import List, Optional
 from pydantic import BaseModel, HttpUrl
 from datetime import date
 
+class Language(BaseModel):
+    tongue: str
+    level: str
 
 class Task(BaseModel):
     task: str
     skills: str
     quantification: Optional[str] = None
-
+    url: Optional[str] = None
 
 class Project(BaseModel):
     name: str
     description: str
+    roles: list[str]
     tasks: List[Task]
 
+class Company(BaseModel):
+    name: str
+    website: Optional[HttpUrl]
+    description: str
+    sectors: List[str]
+    country: str
+    city: str
 
 class WorkExperience(BaseModel):
-    company_name: str
-    company_website: HttpUrl
-    company_description: str
-    company_sectors: List[str]
-    company_country: str
-    company_city: str
+    company: Company 
     date_start: date
     date_end: date
     role_title: str
     role_description: str
     projects: List[Project]
-
 
 class Education(BaseModel):
     grade: str
@@ -42,11 +47,11 @@ class Education(BaseModel):
     field_of_study: List[str]
     tasks: List[str]  # Assuming tasks are simple strings based on _str_to_list usage
 
-
 class Candidate(BaseModel):
     roles: List[str]
     educations: List[Education]
     work_experiences: List[WorkExperience]
+    languages: List[Language]
 
 
 if __name__ == "__main__":
@@ -72,12 +77,14 @@ if __name__ == "__main__":
         ],
         work_experiences=[
             WorkExperience(
-                company_name='Tech Company',
-                company_website='https://www.techcompany.com',
-                company_description='A leading tech company.',
-                company_sectors=['Technology', 'Software'],
-                company_country='Country',
-                company_city='City',
+                company=Company(
+                    name='Tech Company',
+                    website='https://www.techcompany.com',
+                    description='A leading tech company.',
+                    sectors=['Technology', 'Software'],
+                    country='Country',
+                    city='City',
+                ),
                 date_start=date(2020, 6, 1),
                 date_end=date(2024, 1, 31),
                 role_title='Software Engineer',
@@ -86,6 +93,7 @@ if __name__ == "__main__":
                     Project(
                         name='Project A',
                         description='A project to develop a new product.',
+                        roles= ["product owner", "data scientits"],
                         tasks=[
                             Task(
                                 task='Developed a feature',
@@ -96,7 +104,18 @@ if __name__ == "__main__":
                     )
                 ]
             )
+        ],
+        languages=[
+            Language(
+                tongue="french",
+                level="native speaker"
+            ),
+            Language(
+                tongue="english",
+                level="proficient"
+            )
         ]
     )
     # Convert the candidate instance to a JSON string
-    print(candidate)
+    import pprint
+    pprint.pprint(candidate.model_dump())
